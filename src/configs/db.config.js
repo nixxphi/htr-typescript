7c8276fe34039e21844c9542d12b553fb9151d8e
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-25 19:57:21 +0100

    Update db.config.js

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,21 +1,26 @@
-import mongoose from 'mongoose'; 
- import { logger } from '../utils/logger.js'; 
-  
- export default (function database() { 
-     const startdb = () => { 
-         mongoose.set('strictQuery', false) 
-         mongoose.connect(process.env.MONGODB_URI, { 
-             dbName: 'Redcluster' 
-         }) 
-         .then(() => { 
-             logger.info('Database connected successfully...') 
-         }) 
-         .catch(err => { 
-             logger.error('Error connecting to the database:', err) 
-             logger.info('Reconnecting to database...') 
-             startdb() 
-         }) 
-     } 
-  
-     startdb() 
- })
+import mongoose, { ConnectOptions } from 'mongoose';
+import { logger } from '../utils/logger.js';
+
+export default (function database() {
+    const startdb = () => {
+        mongoose.set('strictQuery', false);
+        const options: ConnectOptions = {
+            dbName: 'Redcluster',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true 
+        };
+        mongoose.connect(process.env.MONGODB_URI, options)
+        .then(() => {
+            logger.info('Database connected successfully...');
+        })
+        .catch(err => {
+            logger.error('Error connecting to the database:', err);
+            logger.info('Reconnecting to database...');
+            startdb();
+        });
+    };
+
+    startdb();
+});
