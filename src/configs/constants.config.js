e019958d24339280f0f469b903aaa25c9c261324
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-25 19:55:58 +0100

    Update constants.config.js

--- src/configs/constants.config.js
+++ src/configs/constants.config.js
@@ -1,6 +1,8 @@
-export default { 
-     PAGENOTFOUND: (req, res) => res.json({ 
-         success: false, 
-         message: "You're looking for a resource that does not exist. Try something else." 
-     }) 
- }
+import { Request, Response } from 'express';
+
+export default {
+    PAGENOTFOUND: (req: Request, res: Response) => res.json({ 
+        success: false, 
+        message: "You're looking for a resource that does not exist. Try something else." 
+    }) 
+}
